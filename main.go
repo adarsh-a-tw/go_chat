f8388f8f87a83b31253402cceaf0c99cd24b72af
Adarsh A <adarsh.a@thoughtworks.com>
2024-04-28 18:59:16 +0530

    Adarsh/Aswath : Implement simple chat app

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+
+	router := gin.Default()
+	router.StaticFile("/", "./static/index.html")
+	router.GET("/ws", serveWs)
+	err := router.Run()
+	if err != nil {
+		log.Fatalf("Unable to start server. Error %v", err)
+	}
+	log.Println("Server started successfully.")
+}
+
+func serveWs(c *gin.Context) {
+
+	upgrader := websocket.Upgrader{}
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Printf("Error in upgrading web socket. Error: %v", err)
+		return
+	}
+
+	go handleClient(conn)
+}
+
+var clients = make(map[*websocket.Conn]struct{})
+
+type Message struct {
+	From    string `json:"from"`
+	Message string `json:"message"`
+}
+
+func broadcast(msg Message) {
+	for conn := range clients {
+		conn.WriteJSON(msg)
+	}
+}
+
+func handleClient(c *websocket.Conn) {
+	defer func() {
+		delete(clients, c)
+		log.Println("Closing Websocket")
+		c.Close()
+	}()
+	clients[c] = struct{}{}
+
+	for {
+		var msg Message
+		err := c.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("Error in reading json message. Error : %v", err)
+			return
+		}
+
+		broadcast(msg)
+	}
+}
